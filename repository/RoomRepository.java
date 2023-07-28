7331212a44c98a215bd34e47610146e83ecea3ae
Deepak <deepak@pop-os.localdomain>
2023-07-29 02:21:43 +0530

    room repository small fix

--- repository/RoomRepository.java
+++ repository/RoomRepository.java
@@ -12,11 +12,12 @@ import java.util.List;
 public interface RoomRepository extends JpaRepository<Room,Long> {
 
     @Query("SELECT DISTINCT r FROM Room r LEFT JOIN r.reservations res " +
-            "WHERE (res.checkIn >= :checkOut OR res.checkOut <= :checkIn OR res.status = 'PENDING') " +
+            "WHERE (res.checkIn >= :checkOut OR res.checkOut <= :checkIn AND res.active = false ) " +
             "AND (:roomType IS NULL OR :roomType MEMBER OF r.types)")
     List<Room> findAvailableRooms(
             @Param("checkIn") Timestamp checkIn,
             @Param("checkOut") Timestamp checkOut,
             @Param("roomType") RoomType roomType
     );
+
 }
